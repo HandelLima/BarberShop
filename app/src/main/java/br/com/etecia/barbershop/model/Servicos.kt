94aa5f2c637cf94aeed1a62372b16748b9be7ffd
HandelLima <handel.nlima@gmail.com>
2023-11-15 15:20:59 -0300

    parte 2

---
+++ app/src/main/java/br/com/etecia/barbershop/model/Servicos.kt
@@ -0,0 +1,4 @@
+package br.com.etecia.barbershop.model
+
+class Servicos {
+}
--- app/src/main/java/br/com/etecia/barbershop/view/Home.kt
+++ app/src/main/java/br/com/etecia/barbershop/view/Home.kt
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.etecia.barbershop.R
 import br.com.etecia.barbershop.R.*
+import br.com.etecia.barbershop.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        val_nome = intent.extras?.getString("nome")
+
+        binding.txtNomeUsuario.text ="Bem-vindo(a),$nome"
     }
 }
