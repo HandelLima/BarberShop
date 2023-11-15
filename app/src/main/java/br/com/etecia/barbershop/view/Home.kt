a3b59280f13fd9e3d1c19868a579dcae349d9573
HandelLima <handel.nlima@gmail.com>
2023-11-14 21:45:19 -0300

    teste

---
+++ app/src/main/java/br/com/etecia/barbershop/view/Home.kt
@@ -0,0 +1,13 @@
+package br.com.etecia.barbershop.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.etecia.barbershop.R
+import br.com.etecia.barbershop.R.*
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_home)
+    }
+}
